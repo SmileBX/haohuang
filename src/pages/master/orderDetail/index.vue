ecbb79fc67f98565f0ae2a324172502227f5d869
ldx1992 <928150957@qq.com>
2019-04-22 15:09:26 +0800

    做订单的列表的公共样式

--- src/pages/master/orderDetail/index.vue
+++ src/pages/master/orderDetail/index.vue
@@ -1,5 +1,28 @@
 <template>
-    <div class="container">
-        <div class="bb"></div>
+    <div class="page">
+        <!-- 订单的 -->
+        <div class="column levelPanel">
+            <div class="item">
+                <div class="item__bd">
+                    <div class="box">
+                        <div class="outside">
+                            <div class="pictrueAll">
+                                <div class="pictrue img" style="background-image:url(/static/images/of/a1.png)"></div>
+                            </div>
+                            <div class="txtBox">
+                                <p class="title line2">龙华展涛科技大厦灯箱安装</p>
+                                <p class="time">完成时间：2018-04-28 18:30:15</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import "@/css/dd_style.css";
+export default {
+    
+}
+</script>
