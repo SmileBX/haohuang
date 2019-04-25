6c4fa78a2c7def350312a83dde14b52785462774
liling <907247899@qq.com>
2019-04-25 19:07:59 +0800

    666

---
+++ src/pages/Account/login/index.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="page">
+       <div class="login">
+            <div class="logo">
+                <img class="logoimg" src="/static/images/logo.png">
+            </div> 
+            
+            <button class="login-btn">登录/注册</button>
+            <button class="login-btn btn_gree" @click="loginTel">微信手机号快捷登录</button>
+            <div class="info">
+                <p>若您的微信手机号未注册，将为您直接注册，注册即视为同
+意<span>《浩煌用户注册协议》</span>、<span>《浩煌隐私政策》</span></p>
+            </div>
+        </div>
+        <!-- 授权使用手机号 -->
+        <div class="popModal" v-if="isPop_Tel">
+            <div class="mask"></div>
+            <div class="popup popup-tel">
+                <div class="pop-hd">
+                    <img class="logomin" src="/static/images/logo.png" alt="">
+                    <span>浩煌</span>申请使用</div>
+                <div class="h2">你的手机号码</div>
+                <div class="ul-list mb10">
+                    <div class="li-cell flex">
+                        <div class="li-cell-l">
+                            <span>13526569636</span>
+                            <span>微信绑定号码</span>
+                        </div>
+                        <div class="li-cell-r"><icon type="success_no_circle" size="20"></icon></div>
+                    </div>
+                    <div class="li-cell li-cell-code flex">
+                        <div class="li-cell-l">
+                            <span style="color:#667495">使用其他手机号码</span>
+                        </div>
+                        <div class="li-cell-r"></div>
+                    </div>
+                </div>
+                <div class="footbtns flex">
+                    <div class="btn" @click="cancel">拒绝</div>
+                    <div class="btn btn_gree" @click="confirm">允许</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    onLoad() {
+        this.setBarTitle();
+    },
+    components:{
+      
+    },
+    data() {
+        return {
+           isPop_Tel:false
+        }
+    },
+    methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "登录"
+      });
+    },
+    loginTel(){
+        this.isPop_Tel=true;
+    },
+    cancel: function(){
+        let _this=this;
+        wx.showModal({
+            title: '提示',
+            content: '需要通过授权才能继续，请重新点击并授权！',
+            showCancel: false,
+            confirmColor:'#19ab18',
+                success(res) {
+                    if (res.confirm) {
+                        _this.isPop_Tel=false;
+                    } else if (res.cancel) {
+                    
+                    }
+                }
+        })
+       
+    },
+    confirm: function(){
+        this.isPop_Tel=false;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "./style";
+</style>
---
+++ src/pages/Account/warrant/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="page">
+       <div class="login">
+            <div class="logo">
+                <img class="logoimg" src="/static/images/logo.png">
+            </div> 
+            <div class="auth-text">
+                <text class="main">浩煌小程序正使用浩煌登录提供的服务，浩煌登录将使用：</text>
+                <text class="sub">• 获得你的公开信息（昵称、头像等）</text>
+            </div>
+            <button class="login-btn btn_gree">允许使用</button>
+            <button class="login-btn">取消</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    onLoad() {
+        this.setBarTitle();
+    },
+    components:{
+      
+    },
+    methods: {
+    setBarTitle() {
+      wx.setNavigationBarTitle({
+        title: "登录"
+      });
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "./style";
+</style>
