28c601d70b78302cf97fc0adfdee687dd21ea8ee
bx <996889692@qq.com>
2019-04-26 08:58:05 +0800

    11111

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,6 +18,47 @@ export function formatTime(date) {
     return `${t1} ${t2}`
 }
 
+//API接口地址
+const host = 'http://hhapi.wtvxin.com/Help/';
+//const host = 'http://localhost:801/api/';
+export {
+    host
+}
+//请求封装
+function request(url, method, data, header = {}) {
+    wx.showLoading({
+        title: '加载中' //数据请求前loading
+    })
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: host + url, //仅为示例，并非真实的接口地址
+            method: method,
+            data: data,
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success: function(res) {
+                wx.hideLoading();
+                resolve(res.data)
+            },
+            fail: function(error) {
+                wx.hideLoading();
+                reject(false)
+            },
+            complete: function() {
+                wx.hideLoading();
+            }
+        })
+    })
+}
+
+export function get(url, data) {
+    return request(url, 'GET', data)
+}
+export function post(url, data) {
+    return request(url, 'POST', data)
+}
+
 export function getCurrentPageUrlWithArgs() {
     const pages = getCurrentPages()
     const currentPage = pages[pages.length - 1]
