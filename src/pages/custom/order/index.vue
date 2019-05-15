6efbd7fba1fc6ff0b7f9776a970ce8f24c6a054c
bx <996889692@qq.com>
2019-05-15 10:10:38 +0800

    12

--- src/pages/custom/order/index.vue
+++ src/pages/custom/order/index.vue
@@ -75,7 +75,7 @@
               @click="showCancelOrder(list.Id)"
             >取消订单</div>
             <div class="button active" @click="callService(list.ServiceTel)">联系客服</div>
-            <div class="button active" v-if="list.OrderStatus==2 || list.OrderStatus==3 ||list.OrderStatus==4 || list.OrderStatus==5 || list.OrderStatus==6 || list.OrderStatus==7 || list.OrderStatus==8" @click="seeSchdule(index)">查看进度</div>
+            <div class="button active" v-if="list.OrderStatus==2 || list.OrderStatus==3 ||list.OrderStatus==4 || list.OrderStatus==5 || list.OrderStatus==6 || list.OrderStatus==7" @click="seeSchdule(index)">查看进度</div>
             <div class="button active" style="width:110rpx;color:#fff!important;background:linear-gradient(to right, #fc8556, #ff6666)" v-if="list.OrderStatus===1" @click="orderPay(list.OrderNo)">付款</div>
             <!-- 客服是否确认IsConfirm -->
             <!-- <div class="button active"  v-if="list.IsConfirm==0">修改价格</div> -->
@@ -179,9 +179,10 @@ export default {
     // 客户--订单状态：-1全部，0-待确认，1-待付款，2-处理中，8-待评论
     if (this.$root.$mp.query.typeNo) {
       this.typeNo = this.$root.$mp.query.typeNo * 1;
-    }else{
-      this.typeNo = -1
     }
+    // else{
+    //   this.typeNo = -1
+    // }
     console.log(this.typeNo, "订单状态");
     this.init();
   },
